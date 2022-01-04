b47da6afea6434cf1c0fc2a089070aa3ca907f37
印俊 <3286085684@qq.com>
2022-01-04 15:50:17 +0800

    首页完成

--- entry/src/main/java/com/example/firstapp/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/example/firstapp/slice/MainAbilitySlice.java
@@ -3,12 +3,24 @@ package com.example.firstapp.slice;
 import com.example.firstapp.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.Component;
+import ohos.agp.components.TabList;
 
 public class MainAbilitySlice extends AbilitySlice {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
+
+        TabList tabList = findComponentById(ResourceTable.Id_tab_list);
+        String[] tabListTag={"首页","分类","购物车","我的"};
+        for(int i=0;i<tabListTag.length;i++){
+            TabList.Tab tab = tabList.new Tab(this);
+            tab.setText(tabListTag[i]);
+            tabList.addTab(tab);
+
+        }
+
     }
 
     @Override
